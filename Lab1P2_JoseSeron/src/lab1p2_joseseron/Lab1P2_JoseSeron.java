6a2ae6580549cce0f588a9a2c895ddb6f28235d5
JoseSeron <135990933+JoseSeron@users.noreply.github.com>
2024-01-19 15:17:20 -0600

    Add files via upload

--- Lab1P2_JoseSeron/src/lab1p2_joseseron/Lab1P2_JoseSeron.java
+++ Lab1P2_JoseSeron/src/lab1p2_joseseron/Lab1P2_JoseSeron.java
@@ -1,8 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1p2_joseseron;
+
+import static java.lang.Integer.parseInt;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 /**
  *
@@ -13,8 +17,70 @@ public class Lab1P2_JoseSeron {
     /**
      * @param args the command line arguments
      */
+    static Scanner input = new Scanner(System.in);
+    static Scanner inputInt = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
+
+        ArrayList<Usuario> Database = new ArrayList();
+
+        System.out.println("Laboratorio 1 : Registro de Usuarios\n"
+                + "Jose Roberto Seron\n\n"
+                + "Menu\n"
+                + "1.- Registrar Usuario\n"
+                + "2.- Listar Todo\n"
+                + "3.- Listar Por Dominio");
+
+        int opcionMenu0 = inputInt.nextInt();
+
+        switch (opcionMenu0) {
+            case 1: // Registrar Usuario
+                //Nombre, apellido, fecha de nacimiento, correo, contraseña
+
+                Database = regUsuario(Database);
+                System.out.println(Database);
+
+                break;
+            case 2: // Listar Todo
+
+                break;
+            case 3: // Listar por Dominio
+
+                break;
+            default:
+                throw new AssertionError();
+        }
+
     }
-    
+
+    public static ArrayList regUsuario(ArrayList<Usuario> lista) {
+        System.out.print("Ingrese nombre: ");
+        String nombre = input.nextLine();
+        System.out.print("Ingrese apellido: ");
+        String apellido = input.nextLine();
+        int mes, dia, ano;
+
+        System.out.println("Ingrese fecha de nacimiento (dd/mm/aaaa): ");
+        String fn = input.next();
+        String[] tokens = fn.split("/");
+        dia = Integer.parseInt(tokens[0]);
+        mes = Integer.parseInt(tokens[1]);
+        ano = Integer.parseInt(tokens[2]);
+
+        Date fechaNacimiento = new Date(ano - 1900, mes - 1, dia);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        
+        System.out.print("Ingrese correo: ");
+        String correo = input.next();
+        System.out.print("Ingrese contraseña: ");
+        String contraseña = input.next();
+        
+        Usuario nuevoUsuario = new Usuario(nombre, apellido, correo, contraseña, fechaNacimiento);
+        
+        lista.add(nuevoUsuario);
+   
+          return lista;
+    }
+
 }
---
+++ Lab1P2_JoseSeron/src/lab1p2_joseseron/Usuario.java
@@ -0,0 +1,72 @@
+
+package lab1p2_joseseron;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jose Seron
+ */
+public class Usuario {
+    
+    String nombre, apellido, correo, contraseña;
+    Date fechaNacimiento;
+
+    public Usuario(String nombre, String apellido, String correo, String contraseña, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", correo=" + correo + ", contrase\u00f1a=" + contraseña + ", fechaNacimiento=" + fechaNacimiento + '}';
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+    
+    
+}
