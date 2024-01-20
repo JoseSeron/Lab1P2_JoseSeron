259659275a4bae47689c539d73ab45b13daeb24a
JoseSeron <135990933+JoseSeron@users.noreply.github.com>
2024-01-19 19:38:09 -0600

    Add files via upload

--- Lab1P2_JoseSeron/src/lab1p2_joseseron/Lab1P2_JoseSeron.java
+++ Lab1P2_JoseSeron/src/lab1p2_joseseron/Lab1P2_JoseSeron.java
@@ -25,101 +25,105 @@ public class Lab1P2_JoseSeron {
 
         ArrayList<Usuario> Database = new ArrayList();
         boolean salir = true;
-        
+
         System.out.println("Laboratorio 1 : Registro de Usuarios\n"
                 + "Jose Roberto Seron\n\n");
 
         do {
-            
-        
-        System.out.println("Menu\n"
-                + "1.- Registrar Usuario\n"
-                + "2.- Listar Todo\n"
-                + "3.- Listar Por Dominio");
 
-        int opcionMenu0 = inputInt.nextInt();
+            System.out.println("\nMenu\n"
+                    + "1.- Registrar Usuario\n"
+                    + "2.- Listar Todo\n"
+                    + "3.- Listar Por Dominio");
 
-        switch (opcionMenu0) {
-            case 1: // Registrar Usuario
-                //Nombre, apellido, fecha de nacimiento, correo, contraseña
+            int opcionMenu0 = inputInt.nextInt();
 
-                Database = regUsuario(Database);
-                System.out.println(Database);
+            switch (opcionMenu0) {
+                case 1: // Registrar Usuario
+                    //Nombre, apellido, fecha de nacimiento, correo, contraseña
 
-                break;
-            case 2: // Listar Todo
+                    Database = regUsuario(Database);
+                    System.out.println(Database);
 
-                break;
-            case 3: // Listar por Dominio
+                    break;
+                case 2: // Listar Todo
+                    ListarTodo(Database);
+                    break;
+                case 3: // Listar por Dominio
 
-                break;
-            case 4: // Salir
-salir = false;
-                break;
-            default:
-                throw new AssertionError();
-        }
-} while (salir);
+                    break;
+                case 4: // Salir
+                    salir = false;
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        } while (salir);
+
     }
 
     public static ArrayList regUsuario(ArrayList<Usuario> lista) {
+        
         System.out.print("Ingrese nombre: ");
         String nombre = input.nextLine();
         System.out.print("Ingrese apellido: ");
         String apellido = input.nextLine();
         int mes, dia, ano;
 
-            Date fechaActual = new Date();
-            Date fechaNacimiento;
+        Date fechaActual = new Date();
+        Date fechaNacimiento;
         boolean fechanac = true;
-        
+
         do {
             System.out.println("Ingrese fecha de nacimiento (dd/mm/aaaa): ");
-            String fn = input.next();
+            String fn = input.nextLine();
             String[] tokens = fn.split("/");
             dia = Integer.parseInt(tokens[0]);
             mes = Integer.parseInt(tokens[1]);
             ano = Integer.parseInt(tokens[2]);
 
-            if ( mes>fechaActual.getMonth() && (dia-1)>fechaActual.getDate() ) {
+            if (mes > fechaActual.getMonth() && (dia - 1) > fechaActual.getDate()) {
                 ano++;
             }
-        
+
             fechaNacimiento = new Date(ano - 1900, mes - 1, dia);
             SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd");
 
-            System.out.println("fecah " + fechaActual.getYear());
-            System.out.println("fecah " + fechaNacimiento.getYear());
-            System.out.println("mesactual "+fechaActual.getMonth());
-            System.out.println("mesnac  "+fechaNacimiento.getMonth());
-            System.out.println("diaact "+fechaActual.getDate());
-            System.out.println("dianac "+fechaNacimiento.getDate());
-            
-            
-            int anoact= fechaActual.getYear();
-            int anonac= fechaNacimiento.getYear();
+           
+
+            int anoact = fechaActual.getYear();
+            int anonac = fechaNacimiento.getYear();
             int mesact = fechaActual.getMonth();
             int mesnac = fechaNacimiento.getMonth();
             int diaac = fechaActual.getDate();
             int dianac = fechaNacimiento.getDate();
-            
-            if ((anoact-anonac)>13) {
+
+            if ((anoact - anonac) > 13) {
                 fechanac = false;
             }
-         //   System.out.println("No alcanza la edad minima");
-     
-        } while ( fechanac   );
+            System.out.println("No alcanza la edad minima");
+
+        } while (fechanac);
 
         System.out.print("Ingrese correo: ");
-        String correo = input.next();
+        String correo = input.nextLine();
         System.out.print("Ingrese contraseña: ");
-        String contraseña = input.next();
-        
+        String contraseña = input.nextLine();
+
         Usuario nuevoUsuario = new Usuario(nombre, apellido, correo, contraseña, fechaNacimiento);
-        
+
         lista.add(nuevoUsuario);
-   
-          return lista;
+
+        return lista;
+    }
+
+    public static void ListarTodo(ArrayList<Usuario> lista) {
+        System.out.println("");
+
+        for (int i = 0; i < lista.size() ; i++) {
+            System.out.println(i + 1 + ")" + lista.get(i));
+        }
+        System.out.println("");
     }
 
 }
