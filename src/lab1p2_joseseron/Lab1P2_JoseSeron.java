ed0fe851b8a8088ee2f161af82475816a81ab4bf
Jose R Seron <joserobertoseron@gmail.com>
2024-04-19 15:13:00 -0600

    stackoverflow

--- src/lab1p2_joseseron/Lab1P2_JoseSeron.java
+++ src/lab1p2_joseseron/Lab1P2_JoseSeron.java
@@ -1,89 +1,117 @@
 package lab1p2_joseseron;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  *
- * @author joser
- * fila 3 asiento 6
+ * @author joser fila 3 asiento 6
  */
 public class Lab1P2_JoseSeron {
 
-  
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        
+
         System.out.println("<--------Lab 1 : Jose R. Seron--------->");
-        
-       int tamanioMatriz;
+
+        int tamanioMatriz;
         do {
             System.out.println("Ingrese tamanio de la matriz");
-             tamanioMatriz = input.nextInt();
-             if (tamanioMatriz < 4 || (tamanioMatriz % 2) == 0) {
-                 System.out.println("El tamanio debe ser un numero impar mayor que 4");
+            tamanioMatriz = input.nextInt();
+            if (tamanioMatriz < 4 || (tamanioMatriz % 2) == 0) {
+                System.out.println("El tamanio debe ser un numero impar mayor que 4");
             }
         } while (tamanioMatriz < 4 || (tamanioMatriz % 2) == 0);
-        
-        
+
         int[][] matriz = new int[tamanioMatriz][tamanioMatriz];
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j]=random.nextInt(10);
+                matriz[i][j] = random.nextInt(10);
             }
         }
+
+        System.out.println("Matriz Original");
         imprimirMatriz(matriz);
-        
-        int [] array = matriz[0];
-        
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+" ");
+        System.out.println("");
+
+        int[] array = matriz[0];
+
+        int[][] matrizSorted = new int[tamanioMatriz][tamanioMatriz];
+
+        for (int i = 0; i < matriz.length; i++) {
+            matrizSorted[i] = bubbleSort(matriz[i]);
         }
+
+        System.out.println("Matriz Ordenada");
+        imprimirMatriz(matrizSorted);
         
-        
-        System.out.println("bubblesort");
-        
-        int []arraySorted = bubbleSort(array);
-          for (int i = 0; i < arraySorted.length; i++) {
-            System.out.print(arraySorted[i]+" ");
-        }
+        ArrayList medianas = new ArrayList();
      
+        for (int i = 0; i < matrizSorted.length; i++) {
+            medianas.add(matrizSorted[i][((matrizSorted.length+1)/2)-1]);
+        }
+        
+        
+        System.out.println("\nArreglo Medianas");
+        for (int i = 0; i < medianas.size(); i++) {
+            System.out.print("["+medianas.get(i)+"]");
+        }
+        
+        ArrayList medianasSorted = bubbleSortAL(medianas);
+        
+        System.out.println("Arreglo Medianas Ordenado");
+        for (int i = 0; i < medianasSorted.size(); i++) {
+            System.out.println("["+i+"]");
+        }
+        
+
     }
-    
-    public static int[] bubbleSort (int[] arreglo){
-    System.out.println("entro");
+
+    public static int[] bubbleSort(int[] arreglo) {
+        //  System.out.println("entro");
         for (int i = 0; i < arreglo.length; i++) {
-                
             for (int j = 0; j < arreglo.length; j++) {
-                if (     (j!=arreglo.length-1)  && arreglo[j]>arreglo[j+1] ) {
+                if ((j != arreglo.length - 1) && arreglo[j] > arreglo[j + 1]) {
                     int var = arreglo[j];
-                    arreglo[j]=arreglo[j+1];
-                    arreglo[j+1]=var;
-                    
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = var;
                     bubbleSort(arreglo);
-                    
-                }else {
-                
+                } else {
                 }
             }
-            
         }
-      
         return arreglo;
-        
 
+    }
 
+    
+        public static ArrayList bubbleSortAL(ArrayList arreglo) {
+        //  System.out.println("entro");
+        for (int i = 0; i < arreglo.size(); i++) {
+            for (int j = 0; j < arreglo.size(); j++) {
+                if (   (j != arreglo.size() - 1) && (int)arreglo.get(j) > (int)arreglo.get(j+1) ){
+                    int var = (int)arreglo.get(j);
+                   
+                    arreglo.set(i, arreglo.get(j+1));
+                    arreglo.set(i, var);
+                    bubbleSortAL(arreglo);
+                } else {
+                }
+            }
+        }
+        return arreglo;
 
-        
     }
     
-    public static void imprimirMatriz(int[][]matriz){
+    
+    public static void imprimirMatriz(int[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print("["+matriz[i][j]+"]");
+                System.out.print("[" + matriz[i][j] + "]");
             }
             System.out.println("");
         }
