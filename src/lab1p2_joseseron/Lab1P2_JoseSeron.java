b38d17b3eaa9f3678c0d277aa9eb3685154893b5
Jose R Seron <joserobertoseron@gmail.com>
2024-04-19 13:40:44 -0600

    generacion matriz

--- src/lab1p2_joseseron/Lab1P2_JoseSeron.java
+++ src/lab1p2_joseseron/Lab1P2_JoseSeron.java
@@ -1,8 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package lab1p2_joseseron;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +13,43 @@ public class Lab1P2_JoseSeron {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("<--------Lab 1 : Jose R. Seron--------->");
+        
+       int tamanioMatriz;
+        do {
+            System.out.println("Ingrese tamanio de la matriz");
+             tamanioMatriz = input.nextInt();
+             if (tamanioMatriz < 4 || (tamanioMatriz % 2) == 0) {
+                 System.out.println("El tamanio debe ser un numero impar mayor que 4");
+            }
+        } while (tamanioMatriz < 4 || (tamanioMatriz % 2) == 0);
+        
+        
+        int[][] matriz = new int[tamanioMatriz][tamanioMatriz];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j]=random.nextInt(10);
+            }
+        }
+        
+        imprimirMatriz(matriz);
+        
+        System.out.println("salio while");
+     
     }
     
+    public static void imprimirMatriz(int[][]matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print("["+matriz[i][j]+"]");
+            }
+            System.out.println("");
+        }
+    }
+
 }
